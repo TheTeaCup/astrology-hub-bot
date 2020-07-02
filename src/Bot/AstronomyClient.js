bdbf09451ade9d1a8fa35c8d41de020827d0d148
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2020-07-02 17:56:56 -0400

    Update AstronomyClient.js

--- src/Bot/AstronomyClient.js
+++ src/Bot/AstronomyClient.js
@@ -26,7 +26,6 @@ require("./Handlers/functions")(Bumper);
 
 Bumper.Developers = ["338192747754160138" /* Tea Cup#9999 */];
 Bumper.Staff = ["364007557045551106", "320602199006642178"];
-Bumper.Website = "https://mythical-bots-2.glitch.me";
 
 //require('./Data/Database.js')(Bumper);
 Bumper.Commands = new Discord.Collection();
